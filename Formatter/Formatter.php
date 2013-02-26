210cb762f607d581089a1bd599a875657056de23
Bilal Amarni <bilal.amarni@gmail.com>
2013-02-26 11:20:37 +0100

    throwed exception instead of failing

--- Formatter/Formatter.php
+++ Formatter/Formatter.php
@@ -86,6 +86,10 @@ class Formatter
 
         } else {
             if ($format = $field->get('date_format')) {
+                if (!$value instanceof \DateTime) {
+                    throw new \InvalidArgumentException("'pubDate' should be a DateTime instance.");
+                }
+
                 $value = $value->format($format);
             }
 
